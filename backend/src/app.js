c064ebb57ad5556b3027e6d8ae35d6722b335b61
Hunter <hperky1996@gmail.com>
2023-08-30 19:50:39 -0400

    added cors origin

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,10 @@ const app = express();
 
 app.use(bodyParser.json({limit: '50mb'}));
 app.use(bodyParser.urlencoded({ limit: "50mb", extended: true}));
-app.use(cors());
+app.use("*", cors({
+    origin: "https://mona-lisa-tile.onrender.com/",
+    credentials: true,
+}));
 app.use(express.json());
 
 app.use("/jobs", jobsRouter)
