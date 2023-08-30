01f80de668ffba80a5b7ad883625ca2ffce9fa2a
Hunter <hperky1996@gmail.com>
2023-08-30 19:28:53 -0400

    new db

--- backend/knexfile.js
+++ backend/knexfile.js
@@ -4,8 +4,8 @@ const dotenv = require('dotenv');
 dotenv.config();
 const pg = require("pg")
 const { 
-  DATABASE_URL = "postgres://dvhhnzyu:bGt-MnlE2z0XYYPApNnNUpLC0Eb9JE-o@queenie.db.elephantsql.com/dvhhnzyu",
-  
+  DATABASE_URL = "postgres://sofngvuv:9tlxl9l7LC9-XrBJ35sBUagttWEsQ0mS@lallah.db.elephantsql.com/sofngvuv",
+  DATABASE_DEV = "postgres://dvhhnzyu:bGt-MnlE2z0XYYPApNnNUpLC0Eb9JE-o@queenie.db.elephantsql.com/dvhhnzyu"
 } = process.env;
 
 
@@ -16,6 +16,16 @@ module.exports = {
 
   development: {
     client: "postgresql",
+    connection: DATABASE_DEV,
+    migrations: {
+      directory: path.join(__dirname, "src", "db", "migrations"),
+    },
+    seeds: {
+      directory: path.join(__dirname, "src", "db", 'seeds')
+    }
+  },
+  production: {
+    client: "postgresql",
     connection: DATABASE_URL,
     migrations: {
       directory: path.join(__dirname, "src", "db", "migrations"),
@@ -24,7 +34,6 @@ module.exports = {
       directory: path.join(__dirname, "src", "db", 'seeds')
     }
   },
-
 
 
 };
--- backend/src/db/connection.js
+++ backend/src/db/connection.js
@@ -2,7 +2,7 @@ const path = require("path");
 require("dotenv").config({ path: path.join(__dirname, "..", "..",".env")})
 const env = process.env.NODE_ENV || "development";
 const config = require("../../knexfile")[env];
-console.log(process.env.NODE_ENV)
+// console.log(process.env.NODE_ENV)
 const knex = require("knex")(config);
 
 
